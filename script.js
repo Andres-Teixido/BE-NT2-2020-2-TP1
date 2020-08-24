9779cf09508be8261bd18846b2194d2610e2c379
Andres Teixido Semirozun <andresteix@gmail.com>
2020-08-23 23:28:17 -0300

    Finalizado

--- script.js
+++ script.js
@@ -9,12 +9,39 @@ const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 
+let arrTareas = [];
+
 function addTodo() {
-  //alert('Boton Add TODO clickeado!')
-    let pos = 1;
-    let node = document.createElement("LI");  // Create a <li> node
-    let i = document.createElement("INPUT");
+    //  Creo una tarea y la agrego en arrTareas
+    let tareaNueva = {      
+        estado: false,
+        desc: 'Tarea_' + arrTareas.length
+    };
+    arrTareas.push(tareaNueva);
+
+    let node = document.createElement("LI");
+    let i = document.createElement("input");
     i.setAttribute("type", "checkbox");
+    i.setAttribute("onclick","uncheckedCountSpan.innerHTML = countUnchecked()");
+    var textnode = document.createTextNode(" Tarea " + arrTareas.length);
+
+    //node.appendChild(b);
     node.appendChild(i);
-    list.appendChild(node);     // Append <li> to <ul> with
+    node.appendChild(textnode);
+    list.appendChild(node);
+
+    itemCountSpan.innerHTML = arrTareas.length;   // Cantidad de TAREAS
+    uncheckedCountSpan.innerHTML = countUnchecked();  //  Cantidad de UNCHECKED
+}
+
+function countUnchecked() {
+    let cantCH = 0;
+    let lis = document.getElementsByTagName("input");
+    let cant = lis.length;
+    for (var i = 0; i < cant; i++) { 
+      if(lis[i].checked){
+        cantCH++;
+      }
+    }
+    return cant - cantCH;
 }
